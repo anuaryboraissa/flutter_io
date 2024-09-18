0dd294eaed625b35ca549dd20ad2a6d72e0a882b
anuaryboraissa <anuaryissa21@gmail.com>
2024-09-18 10:10:35 +0300

    vamos 2

---
+++ lib/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_io/audio_player.dart';
+import 'package:flutter_io/record_audio.dart';
+import 'package:flutter_io/video_player.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          const Text(
+            "Flutter IO",
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const AudioPlayerScreen(),
+                ));
+              },
+              child: const Text("Audio Player")),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const VideoPlayerScreen2(),
+                ));
+              },
+              child: const Text("Video Player")),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const AudioRecorderScreen(),
+                ));
+              },
+              child: const Text("Audio Recorder")),
+        ],
+      )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_io/home.dart';
 import 'package:flutter_io/record_audio.dart';
 
 import 'audio_player.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AudioRecorderScreen(),
+      home: const HomeScreen(),
     );
   }
 }
